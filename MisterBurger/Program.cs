99e61fe7b57301d1b926eb1fb951bb45749f4a1b
omayib <omayib@gmail.com>
2019-12-17 20:13:32 +0700

    Add project files.

---
+++ MisterBurger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MisterBurger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
